07e8d85ccbb3fd409a7f5392a96374c3d9b45325
daoohoangg <daohoang2911@gmail.com>
2025-07-08 07:34:46 +0700

    refactor(fix) rebase repository

--- auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
@@ -2,11 +2,10 @@ package com.example.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
+
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example")
-@EnableEurekaServer
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
