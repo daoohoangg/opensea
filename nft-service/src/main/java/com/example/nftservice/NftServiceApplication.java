209f3eb04303950323804238a44e2a1e3bb38ca9
daoohoangg <daohoang2911@gmail.com>
2025-07-13 23:40:03 +0700

    (refactor) file readme

--- nft-service/src/main/java/com/example/nftservice/NftServiceApplication.java
+++ nft-service/src/main/java/com/example/nftservice/NftServiceApplication.java
@@ -2,10 +2,17 @@ package com.example.nftservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+<<<<<<< HEAD
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+=======
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+>>>>>>> 521b5ee (refactor(docker) combine sevices to 1 register)
 public class NftServiceApplication {
 
 	public static void main(String[] args) {
